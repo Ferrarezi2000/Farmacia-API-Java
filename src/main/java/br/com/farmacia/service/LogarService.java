7082bdb740fb8b98d4f075d1b8ba1badc4e28ef3
Thiago Ferrarezi <ferrarezi_alem@yahoo.com.br>
2017-12-22 22:41:24 -0200

    Correções

--- src/main/java/br/com/farmacia/service/LogarService.java
+++ src/main/java/br/com/farmacia/service/LogarService.java
@@ -19,54 +19,60 @@ public class LogarService {
 
     public Farmacia logarFarmacia(String usuario, String senha) {
         Farmacia farmacia = farmaciaRepository.findTopByUsuarioAcessoAndSenhaAcesso(usuario, senha);
+        if (farmacia != null) {
 
-        List<Avaliacao> avaliacoesTotal = avaliacaoRepository.findAllByFarmacia(farmacia);
-        Double totalSoma = avaliacoesTotal.stream().mapToDouble(a -> a.getValor()).sum();
-        Double media = totalSoma / avaliacoesTotal.size();
-        farmacia.setMedia(Math.round(media / 0.5) * 0.5);
+            List<Avaliacao> avaliacoesTotal = avaliacaoRepository.findAllByFarmacia(farmacia);
+            Double totalSoma = avaliacoesTotal.stream().mapToDouble(a -> a.getValor()).sum();
+            Double media = totalSoma / avaliacoesTotal.size();
+            farmacia.setMedia(Math.round(media / 0.5) * 0.5);
 
-        List<Avaliacao> avaliacoes = avaliacaoRepository.findAllByFarmaciaAndComentarioNotNullAndRespostaIsNull(farmacia);
-        avaliacoes.forEach(avaliacao -> avaliacao.setFarmacia(null));
-        farmacia.setAvaliacoes(avaliacoes);
-        farmacia.setAdministrador(null);
+            List<Avaliacao> avaliacoes = avaliacaoRepository.findAllByFarmaciaAndComentarioNotNullAndRespostaIsNull(farmacia);
+            avaliacoes.forEach(avaliacao -> avaliacao.setFarmacia(null));
+            farmacia.setAvaliacoes(avaliacoes);
+            farmacia.setAdministrador(null);
 
-        List<Contato> contatos = contatoRepository.findAllByFarmacia(farmacia);
-        contatos.forEach(contato -> contato.setFarmacia(null));
-        farmacia.setContatos(contatos);
+            List<Contato> contatos = contatoRepository.findAllByFarmacia(farmacia);
+            contatos.forEach(contato -> contato.setFarmacia(null));
+            farmacia.setContatos(contatos);
 
-        Endereco endereco = enderecoRepository.findTopByFarmacia(farmacia);
-        endereco.setFarmacia(null);
-        farmacia.setEndereco(endereco);
+            Endereco endereco = enderecoRepository.findTopByFarmacia(farmacia);
+            endereco.setFarmacia(null);
+            farmacia.setEndereco(endereco);
 
-        List<Adicional> adicionais = adicionalRepository.findAllByFarmacia(farmacia);
-        adicionais.forEach(adicional -> adicional.setFarmacia(null));
-        farmacia.setListaAdicionais(adicionais);
-        return farmacia;
+            List<Adicional> adicionais = adicionalRepository.findAllByFarmacia(farmacia);
+            adicionais.forEach(adicional -> adicional.setFarmacia(null));
+            farmacia.setListaAdicionais(adicionais);
+            return farmacia;
+        }
+        return null;
     }
 
     public Patrocinador logarPatrocinador(String usuario, String senha) {
         Patrocinador patrocinador = patrocinadorRepository.findTopByUsuarioAcessoAndSenhaAcesso(usuario, senha);
 
-        List<Avaliacao> avaliacoesTotal = avaliacaoRepository.findAllByPatrocinador(patrocinador);
-        Double totalSoma = avaliacoesTotal.stream().mapToDouble(a -> a.getValor()).sum();
-        Double media = totalSoma / avaliacoesTotal.size();
-        patrocinador.setMedia(Math.round(media / 0.5) * 0.5);
+        if (patrocinador != null) {
+            List<Avaliacao> avaliacoesTotal = avaliacaoRepository.findAllByPatrocinador(patrocinador);
+            Double totalSoma = avaliacoesTotal.stream().mapToDouble(a -> a.getValor()).sum();
+            Double media = totalSoma / avaliacoesTotal.size();
+            patrocinador.setMedia(Math.round(media / 0.5) * 0.5);
 
-        List<Avaliacao> avaliacoes = avaliacaoRepository.findAllByPatrocinadorAndComentarioNotNullAndRespostaIsNull(patrocinador);
-        avaliacoes.forEach(avaliacao -> avaliacao.setPatrocinador(null));
-        patrocinador.setAvaliacoes(avaliacoes);
+            List<Avaliacao> avaliacoes = avaliacaoRepository.findAllByPatrocinadorAndComentarioNotNullAndRespostaIsNull(patrocinador);
+            avaliacoes.forEach(avaliacao -> avaliacao.setPatrocinador(null));
+            patrocinador.setAvaliacoes(avaliacoes);
 
-        List<Contato> contatos = contatoRepository.findAllByPatrocinador(patrocinador);
-        contatos.forEach(contato -> contato.setPatrocinador(null));
-        patrocinador.setContatos(contatos);
+            List<Contato> contatos = contatoRepository.findAllByPatrocinador(patrocinador);
+            contatos.forEach(contato -> contato.setPatrocinador(null));
+            patrocinador.setContatos(contatos);
 
-        Endereco endereco = enderecoRepository.findTopByPatrocinador(patrocinador);
-        endereco.setPatrocinador(null);
-        patrocinador.setEndereco(endereco);
+            Endereco endereco = enderecoRepository.findTopByPatrocinador(patrocinador);
+            endereco.setPatrocinador(null);
+            patrocinador.setEndereco(endereco);
 
-        List<Adicional> adicionais = adicionalRepository.findAllByPatrocinador(patrocinador);
-        adicionais.forEach(adicional -> adicional.setFarmacia(null));
-        patrocinador.setListaAdicionais(adicionais);
-        return patrocinador;
+            List<Adicional> adicionais = adicionalRepository.findAllByPatrocinador(patrocinador);
+            adicionais.forEach(adicional -> adicional.setPatrocinador(null));
+            patrocinador.setListaAdicionais(adicionais);
+            return patrocinador;
+        }
+        return null;
     }
 }
